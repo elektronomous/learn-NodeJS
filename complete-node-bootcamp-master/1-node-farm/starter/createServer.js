600d15efb4f18600aabf68e037a595608f9c9ebc
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2023-01-18 00:24:05 +0800

    200 respond

--- complete-node-bootcamp-master/1-node-farm/starter/createServer.js
+++ complete-node-bootcamp-master/1-node-farm/starter/createServer.js
@@ -1,4 +1,6 @@
 const http = require("http");
+// we're going to create simple API
+const fs = require("fs");
 
 // set how the server receive the request and
 // sending the response
@@ -10,6 +12,12 @@ const server = http.createServer((req, res) => {
     res.end("This is OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is PRODUCT");
+  } else if (pathName === "/api") {
+    // __dirname will always translate to directory in which this script is reside
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      console.log(JSON.parse(data));
+    });
+    res.end("API");
   } else {
     res.writeHead(404, {
       "Content-type": "text/html", // expect the browser to receive html file
